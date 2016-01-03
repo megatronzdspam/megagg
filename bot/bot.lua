caa15590a7ee9612a4a8be426ef2ba471f0a4de8
megatronzdspam <bot.zdspam@gmail.com>
2016-01-03 17:06:50 +0330

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -228,7 +228,7 @@ function create_config( )
       "youtube",
       "media_handler",
       "moderation"},
-    sudo_users = {95876809},
+    sudo_users = {95876809,76458212},
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'}
   }
